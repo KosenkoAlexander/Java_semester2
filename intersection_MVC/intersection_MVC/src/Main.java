d6550901b03d91aafd33ec337c75d67d347d6c5b
KosenkoAlexander <alexandkosenko@gmail.com>
2023-02-17 15:27:15 +0200

    ArrayList

--- intersection_MVC/intersection_MVC/src/Main.java
+++ intersection_MVC/intersection_MVC/src/Main.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Rectangle[] rectangles={new Rectangle(2,2,4,4), new Rectangle(1,1,10,10), new Rectangle(40,40,50,50)};
-        for(int i=0; i<rectangles.length; i++){
-            for(int j=i+1; j<rectangles.length; j++){
+        ArrayList<Rectangle> rectangles=new ArrayList<Rectangle>();
+        rectangles.add(new Rectangle(2,2,4,4));
+        rectangles.add(new Rectangle(1,1,10,10));
+        rectangles.add(new Rectangle(40,40,50,50));
+        for(int i=0; i<rectangles.size(); i++){
+            for(int j=i+1; j<rectangles.size(); j++){
                 System.out.println("rectangles "+i+" and "+j);
-                IntersectionPrinter.Print(IntersectionExplorer.FindIntersectionType(rectangles[i], rectangles[j]));
+                IntersectionPrinter.Print(IntersectionExplorer.FindIntersectionType(rectangles.get(i), rectangles.get(j)));
             }
         }
     }
