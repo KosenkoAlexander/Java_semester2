a39d6344f3e2c4a8c94df8abdab87ec1217c33d0
KosenkoAlexander <alexandkosenko@gmail.com>
2023-03-17 14:32:47 +0200

    almost done

--- multiInheritance/src/Main.java
+++ multiInheritance/src/Main.java
@@ -2,8 +2,26 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Smartphone s=new Smartphone();
-        Smartphone.super1.process();
+        ArrayList<Phone> phones=new ArrayList<>();
+        phones.add(new Phone());
+        phones.add(new Phone());
+        phones.add(new Phone(){
+            private Smartphone s=new Smartphone();
+            public void process(){
+                s.process();
+            }
+        });
+        ArrayList<Computer> computes=new ArrayList<>();
+        computes.add(new Computer());
+        computes.add(new Computer());
+        computes.add(new Computer(){
+            private Smartphone s=new Smartphone();
+            public void process(){
+                s.process();
+            }
+        });
+        phones.forEach(p->p.process());
+        computes.forEach(c->c.process());
     }
 }
 
@@ -18,8 +36,8 @@ class Computer{
     }
 }
 class Smartphone{
-    class super1 extends Phone{}
-    private Computer super2=new Computer(){};
+    private final Phone super1=new Phone();
+    private final Computer super2=new Computer();
     public void process(){
         super1.process();
         super2.process();
