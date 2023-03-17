2f762645b6f1011720ee01ef9de4522c8b354354
KosenkoAlexander <alexandkosenko@gmail.com>
2023-03-17 14:40:38 +0200

    object identities

--- multiInheritance/src/Main.java
+++ multiInheritance/src/Main.java
@@ -3,19 +3,19 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Phone> phones=new ArrayList<>();
-        phones.add(new Phone());
-        phones.add(new Phone());
-        phones.add(new Phone(){
-            private Smartphone s=new Smartphone();
+        phones.add(new Phone("p1"));
+        phones.add(new Phone("p2"));
+        phones.add(new Phone("s1"){
+            private Smartphone s=new Smartphone(this.getName());
             public void process(){
                 s.process();
             }
         });
         ArrayList<Computer> computes=new ArrayList<>();
-        computes.add(new Computer());
-        computes.add(new Computer());
-        computes.add(new Computer(){
-            private Smartphone s=new Smartphone();
+        computes.add(new Computer("c1"));
+        computes.add(new Computer("c2"));
+        computes.add(new Computer("s2"){
+            private Smartphone s=new Smartphone(this.getName());
             public void process(){
                 s.process();
             }
@@ -26,18 +26,36 @@ public class Main {
 }
 
 class Phone{
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public Phone(String name){
+        this.name=name;
+    }
     public void process(){
-        System.out.println("setting network...");
+        System.out.println(name+" setting network...");
     }
 }
 class Computer{
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public Computer(String name){
+        this.name=name;
+    }
     public void process(){
-        System.out.println("activating OS...");
+        System.out.println(name+" activating OS...");
     }
 }
 class Smartphone{
-    private final Phone super1=new Phone();
-    private final Computer super2=new Computer();
+    private final Phone super1;
+    private final Computer super2;
+    public Smartphone(String name){
+        super1=new Phone(name);
+        super2=new Computer(name);
+    }
     public void process(){
         super1.process();
         super2.process();
