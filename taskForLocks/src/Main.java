0a75d8cfe7f06943f34893a8c26fb08b727fa57f
KosenkoAlexander <alexandkosenko@gmail.com>
2023-04-24 02:33:43 +0300

    initial

---
+++ taskForLocks/src/Main.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+        initialGenerator(false, 1000);
+        Counter c1=new Counter(new File("numbers0"), "count1");
+        Counter c2=new Counter(new File("numbers1"), "count2");
+        Counter c3=new Counter(new File("numbers2"), "count3");
+        c1.start();
+        c2.start();
+        c3.start();
+        c1.join();
+        c2.join();
+        c3.join();
+        System.out.println(Counter.getCount());
+    }
+    public static void initialGenerator(boolean deleteAfterWork, int len){
+        for(int i=0; i<3; i++){
+            File file=new File("numbers"+i);
+            if(deleteAfterWork)file.deleteOnExit();
+            if(!file.exists()){
+                Random rnd=new Random();
+                FileWriter writer;
+                try{
+                    file.createNewFile();
+                    writer=new FileWriter(file);
+                    try{
+                        for(int j=0; j<len; j++) {
+                            writer.write("" + rnd.nextInt(0, 10));
+                        }
+                    }finally {
+                        writer.close();
+                    }
+
+                }catch (IOException e){
+                    System.out.println("troubles with files");
+                }
+
+            }
+        }
+    }
+}
+class Counter extends Thread{
+    private static int count=0;
+    public static int getCount(){
+        return count;
+    }
+    private File file;
+    private String name;
+    public Counter(File file, String name){
+        this.file=file;
+        this.name=name;
+    }
+    private static Lock lock=new ReentrantLock();
+    public void run(){
+        try{
+            while(!lock.tryLock()){
+                System.out.println(name+" LOCKED");
+            }
+
+            FileReader r=new FileReader(file);
+            BufferedReader reader=new BufferedReader(r);
+
+            try {
+                while (reader.ready()){
+                    char c=(char)reader.read();
+                    if(Character.isDigit(c))count+=Character.digit(c, 10);
+                }
+            }finally {
+                r.close();
+                reader.close();
+            }
+
+        }catch(IOException e){
+            System.out.println("IOException");
+            e.printStackTrace();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+}
